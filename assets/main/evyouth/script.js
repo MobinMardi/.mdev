cb5563acdc0df79cc137361660f6495a26cbaa4e
Mobin Mardi <165613890+MobinMardi@users.noreply.github.com>
2025-03-02 18:12:03 +0330

    Updated EV Youth Tab UI

--- assets/main/evyouth/script.js
+++ assets/main/evyouth/script.js
@@ -1,4 +1,3 @@
-// Function to open the pop-up with the player's agent information
 function showPopup(agents) {
     const agentsContainer = document.getElementById('agents-container');
     agentsContainer.innerHTML = ''; // Clear previous content
@@ -16,34 +15,30 @@ function showPopup(agents) {
     const popup = document.getElementById('popup');
     const popupContent = document.querySelector('.popup-content');
     popup.style.display = 'block';
-    popupContent.classList.remove('fade-out'); // Ensure fade-out class is removed
-    document.body.classList.add('body-no-scroll'); // Disable scrolling
+    popupContent.classList.remove('fade-out');
+    document.body.classList.add('body-no-scroll');
 }
 
-// Function to close the pop-up
 function closePopup() {
     const popupContent = document.querySelector('.popup-content');
-    popupContent.classList.add('fade-out'); // Trigger the fade-out animation
+    popupContent.classList.add('fade-out');
     setTimeout(() => {
         document.getElementById('popup').style.display = 'none';
-        popupContent.classList.remove('fade-out'); // Reset the fade-out class
-        document.body.classList.remove('body-no-scroll'); // Enable scrolling
-    }, 250); // Match this duration to the animation duration
+        popupContent.classList.remove('fade-out');
+        document.body.classList.remove('body-no-scroll');
+    }, 250);
 }
 
-// Event listeners for player boxes
 document.addEventListener('DOMContentLoaded', (event) => {
     const popup = document.getElementById('popup');
     const popupContent = document.querySelector('.popup-content');
 
-    // Event listener for clicks outside the popup
     popup.addEventListener('mousedown', (event) => {
         if (event.target === popup) {
             closePopup();
         }
     });
 
-    // Event listeners for player boxes
     const playerBoxes = document.querySelectorAll('.player-box');
     playerBoxes.forEach(box => {
         if (!box.querySelector('h3').innerText.includes('Coach')) {
